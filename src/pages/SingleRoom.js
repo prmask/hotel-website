b817d3aea8e0abed4636c56256be22e0c22ac75e
Prem Asok <prmask@gmail.com>
2021-02-01 04:55:43 +0530

    Hero section

---
+++ src/pages/SingleRoom.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class SingleRoom extends Component {
+	render() {
+		return (
+			<div>
+				<h2>Single Room Page</h2>
+			</div>
+		);
+	}
+}
